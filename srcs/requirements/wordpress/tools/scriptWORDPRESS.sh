00ea109d45fe2f19d101ce11345496fb60e435cc
Stelirion <noegennaro73@gmail.com>
2024-01-26 10:50:46 -0500

    fix all

--- srcs/requirements/wordpress/tools/scriptWORDPRESS.sh
+++ srcs/requirements/wordpress/tools/scriptWORDPRESS.sh
@@ -13,10 +13,8 @@ sed -i -r "s/localhost/mariadb/1"    wp-config.php
 
 
 wp core install --url="$DOMAIN_NAME/" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
-    
 
 wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --allow-root
-
 
 wp theme install twentytwenty --activate --allow-root
 
@@ -25,7 +23,7 @@ wp post delete $(wp post list --post_type=page --format=ids --allow-root) --allo
 
 wp plugin update --all --allow-root
 
-wp post create --post_type='post' --post_status=publish --post_title='UwU' --post_content="Salut j'aime les pates et le tiramisu" --post_thumbnail_id=7 --post_author=2 --allow-root
+wp post create --post_type='post' --post_status=publish --post_title='Inception' --post_content="Un projet de kaliter" --post_thumbnail_id=7 --post_author=2 --allow-root
  
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
@@ -33,5 +31,4 @@ mkdir /run/php
 
 chown -R www-data:www-data ./
 
-# tail -f /dev/null
 /usr/sbin/php-fpm7.4 -F
