239d9de1478413a5e1a81a7003e489324282dac4
Stelirion <noegennaro73@gmail.com>
2024-02-22 05:45:20 -0500

    push

--- srcs/requirements/wordpress/tools/scriptWORDPRESS.sh
+++ srcs/requirements/wordpress/tools/scriptWORDPRESS.sh
@@ -6,7 +6,7 @@ wp core download --allow-root
 
 wp config create --dbname="wordpress_DB" --dbuser="$DB_USER" --dbpass="$DB_USER_PASSWORD" --dbhost="$DB_HOST" --allow-root
 
-wp core install --url="$DOMAIN_NAME/" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
+wp core install --url="ngennaro@42.fr/" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
 
 wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --allow-root
 wp user create "$WP_ADMIN" "$WP_ADMIN_EMAIL" --role=administrator --user_pass="$WP_ADMIN_PASSWORD" --allow-root
